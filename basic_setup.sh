947f746f0f7f532b7a8668d58e95c94f8de3fa60
fdiazsmith <fdiazsmith@gmail.com>
2019-02-14 10:47:10 +0100

    basic question structure

--- basic_setup.sh
+++ basic_setup.sh
@@ -1,1 +1,11 @@
 echo "Starting basic set up"
+INSTALL_NODE=false
+INSTALL_GIT=true
+while true; do
+    read -p "Do you wish to install this program?" yn
+    case $yn in
+        [Yy]* ) make install; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
