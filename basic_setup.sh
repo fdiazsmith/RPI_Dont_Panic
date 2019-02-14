8232b54e102a5a0f683f3a91ecd2c2cb363aa3bb
fdiazsmith <fdiazsmith@gmail.com>
2019-02-14 10:58:34 +0100

    a bit more structured

--- basic_setup.sh
+++ basic_setup.sh
@@ -1,11 +1,27 @@
 echo "Starting basic set up"
 INSTALL_NODE=false
 INSTALL_GIT=true
-while true; do
-    read -p "Do you wish to install this program?" yn
-    case $yn in
-        [Yy]* ) echo "got it" ; break;;
-        [Nn]* ) echo "cool you do it"  exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+
+ECHO "This will take care of installing the basic stuff you would expect\n but we would like to ask a few basic questions first."
+
+function getUserInput {
+  #while true; do
+      read -p "Do you wish to install git?" yn
+      case $yn in
+          [Yy]* ) INSTALL_GIT=true; echo "got it" ; break;;
+          [Nn]* ) INSTALL_GIT=false; echo "cool you do it"  exit;;
+          * ) echo "Please answer yes or no.";;
+      esac
+      read -p "Do you wish to install node?" yn
+      case $yn in
+          [Yy]* ) INSTALL_NODE=true;echo "got it" ; break;;
+          [Nn]* ) INSTALL_NODE=false;echo "cool you do it"  exit;;
+          * ) echo "Please answer yes or no.";;
+      esac
+  #done
+}
+
+getUserInput
+
+echo "Value for installing node:"$INSTALL_NODE
+echo "Value for installing git:"$INSTALL_GIT
